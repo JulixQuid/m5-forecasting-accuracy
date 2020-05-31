7df7da7c5dd364bb78b5d790a327b165354b92ec
Brown-Energy <errecastillo@gmail.com>
2020-05-30 22:44:56 -0500

    lo que hice : 30/05/2020 22:44:56

---
+++ model.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np
+
+
+calendario  = pd.read_csv("calendar.csv")
+sales_train_validation =  pd.read_csv("sales_train_validation.csv")
+sell_prices = pd.read_csv("sell_prices.csv")
+sample_submission = pd.read_csv("sample_submission.csv")
+
+
