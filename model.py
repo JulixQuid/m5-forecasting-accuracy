79f1b608840770724b2a84d0449685ac90a7149a
Brown-Energy <errecastillo@gmail.com>
2020-05-30 22:59:11 -0500

    elimine model : 30/05/2020 22:59:11

--- model.py
+++ model.py
@@ -7,5 +7,3 @@ sales_train_validation =  pd.read_csv("sales_train_validation.csv")
 sell_prices = pd.read_csv("sell_prices.csv")
 sample_submission = pd.read_csv("sample_submission.csv")
 
-def fun():
-    return True
