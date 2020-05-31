f2ac7e9a6a9ccef944b1752fadbe0c2a701e0684
Brown-Energy <errecastillo@gmail.com>
2020-05-30 23:24:18 -0500

    problema entendido con julian : 30/05/2020 23:24:18

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 import pandas as pd 
 import numpy as np
+import os
 
 
 calendario  = pd.read_csv("calendar.csv")
 sales_train_validation =  pd.read_csv("sales_train_validation.csv")
 sell_prices = pd.read_csv("sell_prices.csv")
 sample_submission = pd.read_csv("sample_submission.csv")
+
 
